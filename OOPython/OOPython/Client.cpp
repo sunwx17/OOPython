92e04bce687955aab202cc12ce6b1c8cb167d31c
sunwx17 <35945622+sunwx17@users.noreply.github.com>
2018-05-29 20:39:48 +0800

    init

---
+++ OOPython/OOPython/Client.cpp
@@ -0,0 +1,62 @@
+//Client.cpp
+#include "Client.h"
+
+Client::Client(istream& _in) : in(_in) {}
+
+bool Client::getLine(string &s)
+{
+	bool notEOF = !(getline(in, s).eof());
+    return notEOF;
+}
+
+void Client::work()
+{
+	bool flagExit = false;
+    while (!flagExit)
+    {
+        string line;
+		bool endOfFile = !(getLine(line));
+		if (endOfFile) {
+			break;
+		}
+		unsigned int numOfTab = 0;
+		bool tabCorrect = scan(numOfTab, line);//先默认correct
+		if (!tabCorrect) {
+			cout << "缩进错误" << endl;
+			continue;
+		}
+		//do something with "numOfTab" and "line"（前者为缩进数，后者为不带缩进的行）
+
+    }
+    return;
+}
+
+bool Client::scan(unsigned int &num, string &line) {
+	bool tabCorrect = true;
+	num = 0;
+	while ((line.begin() != line.end()) && tabCorrect) {
+		if (line[0] == '\t') {
+			num++;
+			line.erase(line.begin());
+		}
+		else if (line[0] == ' ') {
+			for (int i = 1; i < 4; i++) {
+				if (line[i] != ' ') {
+					tabCorrect = false;
+					break;
+				}
+			}
+			if (tabCorrect) {
+				num++;
+				for (int i = 0; i < 4; i++) {
+					line.erase(line.begin());
+				}
+			}
+		}
+		else {
+			break;
+		}
+	}
+	cout << line << endl << num << endl;
+	return tabCorrect;
+}
---
+++ OOPython/OOPython/main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Client.h"
+//#include "Data.h"
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (argc > 1) {
+		freopen(argv[1], "r", stdin);
+	}
+	Client client(cin);
+	client.work();
+	return 0;
+}
