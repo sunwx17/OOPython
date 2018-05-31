45d0db35459d1df5a91934021a8857f9da455035
FztTony <tony_tiny@126.com>
2018-05-31 17:35:24 +0800

    pyObject and pyObjectData

--- OOPython/OOPython/Data.h
+++ OOPython/OOPython/Data.h
@@ -8,8 +8,8 @@
 #include <memory>
 using namespace std;
 
-class pyObject;
-typedef shared_ptr<const pyObject> pyObjectPtr;
+class pyObjectData;
+typedef shared_ptr<const pyObjectData> pyObjectPtr;
 
 class pyObjectBool;
 class pyObjectInt;
@@ -26,22 +26,23 @@ public:
 	pyObject();
 	virtual ~pyObject() = 0;
 	virtual string getType() const = 0;
-	virtual pyObjectData* work() = 0;
 	virtual void print() = 0;
 };
 
+//以下pyObjectData类
 class pyObjectData :public pyObject {
-	virtual pyObjectData* operatorFather(const pyObject *other, const char* ope) const = 0;
-	virtual pyObjectData* operatorFatherBool(const pyObject *other, const char* ope) const = 0;
+	virtual pyObjectData* operatorFather(const pyObjectData *other, const char* ope) const = 0;
+	pyObjectData* operatorFatherBool(const pyObjectData *other, const char* ope) const;
+	virtual float getDataFloat() = 0;
 public:
 	friend class pyObjectBool;
-	friend class pyObjectInt;
+	friend class pyObjectInt;//Mark:把它去掉
 	pyObjectData();
 	virtual ~pyObjectData() = 0;
 	virtual string getType() const = 0;
 	pyObjectData* operator+(const pyObjectData *other) const;
 	pyObjectData* operator-(const pyObjectData *other) const;
-	virtual pyObjectData* operator-() const = 0;//Mark: To Do: 实现基类operator
+	virtual pyObjectData* operator-() const = 0;
 	pyObjectData* operator*(const pyObjectData *other) const;
 	pyObjectData* operator/(const pyObjectData *other) const;
 	pyObjectData* operator==(const pyObjectData *other) const;
@@ -49,8 +50,6 @@ public:
 	pyObjectData* operator>(const pyObjectData *other) const;
 	pyObjectData* operator<<(const pyObjectData *other) const;
 	pyObjectData* operator>>(const pyObjectData *other) const;
-	virtual pyObjectData* work() = 0;
-
 };
 //class pyObjectFunc : public pyObject {
 //	map<string, pyObjectData*> dataFunc;
@@ -65,36 +64,39 @@ class pyObjectBool : public pyObjectData {
 	pyObjectBool(const pyObjectBool &) = delete;
 	void operator=(const pyObjectBool &) = delete;
 	~pyObjectBool() {}
-	pyObjectData* operatorFather(const pyObject *other, const char* ope) const;
-	pyObjectData* operatorFatherBool(const pyObject *other, const char* ope) const;
+	pyObjectData* operatorFather(const pyObjectData *other, const char* ope) const;
+	pyObjectData* operatorFatherBool(const pyObjectData *other, const char* ope) const;
+	float getDataFloat();
 public:
 	static pyObjectBool trueBool;
 	static pyObjectBool falseBool;
 	string getType() const;
 	pyObjectData* operator-() const;
-	pyObjectData* work();
+	void print();
 };
 
-class pyObjectInt : public pyObject {
+class pyObjectInt : public pyObjectData {
 	int data;
-	pyObjectData* operatorFather(const pyObject *other, const char* ope) const;
-	pyObjectData* operatorFatherBool(const pyObject *other, const char* ope) const;
+	pyObjectData* operatorFather(const pyObjectData *other, const char* ope) const;
+	pyObjectData* operatorFatherBool(const pyObjectData *other, const char* ope) const;
+	float getDataFloat();
 public:
 	pyObjectInt(int _data);
 	string getType() const;
 	pyObjectData* operator-() const;
-	pyObjectData* work();
+	void print();
 };
 
-class pyObjectFloat : public pyObject {
+class pyObjectFloat : public pyObjectData {
 	float data;
-	pyObjectData* operatorFather(const pyObject *other, const char* ope) const;
-	pyObjectData* operatorFatherBool(const pyObject *other, const char* ope) const;
+	pyObjectData* operatorFather(const pyObjectData *other, const char* ope) const;
+	pyObjectData* operatorFatherBool(const pyObjectData *other, const char* ope) const;
+	float getDataFloat();
 public:
 	pyObjectFloat(float _data);
 	string getType() const;
 	pyObjectData* operator-() const;
-	pyObjectData* work();
+	void print();
 };
 
 #endif
