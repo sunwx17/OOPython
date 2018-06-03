47b7da37db5098f8b905f5d0004d693128bc708e
sunwx17 <35945622+sunwx17@users.noreply.github.com>
2018-06-03 13:25:08 +0800

    range(1,3,2)

--- OOPython/OOPython/Buildin.cpp
+++ OOPython/OOPython/Buildin.cpp
@@ -3,16 +3,18 @@
 
 pyObjectPtr range(vector<pyObjectPtr> elems) {
 	pyObjectContainerPtr ocp = new pyObjectList;
-	int start, end;
-	if (elems.size() == 2) {
+	int start = 0, end, len = 1;
+	if (elems.size() >= 2) {
 		start = dynamic_cast<pyObjectInt*>(elems[0])->operator int();
 		end = dynamic_cast<pyObjectInt*>(elems[1])->operator int();
+		if (elems.size() == 3) {
+			len = dynamic_cast<pyObjectInt*>(elems[2])->operator int();
+		}
 	}
-	else {
-		start = 0;
+	else if (elems.size() == 1){
 		end = dynamic_cast<pyObjectInt*>(elems[0])->operator int();
 	}
-	for (int i = start; i < end; i++) {
+	for (int i = start; i < end; i += len) {
 		pyObjectDataPtr odp = new pyObjectInt(i);
 		ocp->pushBack(odp);
 	}
